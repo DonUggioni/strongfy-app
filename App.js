293fbc90e3aea8bbd67ef7578b203c4325f18642
DonUggioni <renan.uggioni@gmail.com>
2023-01-27 22:16:08 +0000

    Fixed strange behaviour in navigation

--- App.js
+++ App.js
@@ -29,195 +29,192 @@ const customFonts = {
   'open-sans-bold': require('./assets/fonts/OpenSans/OpenSans-Bold.ttf'),
 };
 
-function RootApp() {
-  const { userIsAuthenticated } = useAppContext();
-  const [fontsLoaded] = useFonts(customFonts);
+function AuthenticationStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.background },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+          fontFamily: 'open-sans-semi-bold',
+        },
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name='Login' component={LoginScreen} />
+      <Stack.Screen name='Signup' component={SignUpScreen} />
+    </Stack.Navigator>
+  );
+}
 
-  function AuthenticationStack() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: GlobalStyles.colors.background },
-            headerTintColor: 'white',
-            headerTitleStyle: {
-              fontFamily: 'open-sans-semi-bold',
-            },
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name='Login' component={LoginScreen} />
-          <Stack.Screen name='Signup' component={SignUpScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+function TrainingOptionsStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.background },
+        headerTintColor: 'white',
+        headerTitleStyle: {
+          fontFamily: 'open-sans-semi-bold',
+        },
+      }}
+    >
+      <Stack.Screen
+        name='WorkoutsScreen'
+        component={WorkoutsScreen}
+        options={{ title: 'Workouts' }}
+      />
+      <Stack.Screen
+        name='SelectPhase'
+        component={BlockOptions}
+        options={{ title: 'Training Phase' }}
+      />
+      <Stack.Screen
+        name='WorkoutSelection'
+        component={WorkoutSelection}
+        options={{
+          title: 'Select a workout',
+        }}
+      />
+      <Stack.Screen
+        name='SelectDay'
+        component={SelectDay}
+        options={{ title: 'Select Day' }}
+      />
+      <Stack.Screen name='WorkoutOfTheDay' component={WorkoutOfTheDay} />
 
-  function TrainingOptionsStack() {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: { backgroundColor: GlobalStyles.colors.background },
-          headerTintColor: 'white',
-          headerTitleStyle: {
-            fontFamily: 'open-sans-semi-bold',
+      <Stack.Screen
+        name='PreviewModal'
+        component={PreviewModal}
+        options={{ presentation: 'modal', title: false }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AppNavigation() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.background },
+        headerTintColor: 'white',
+        tabBarStyle: {
+          backgroundColor: GlobalStyles.colors.primary700,
+          paddingTop: 5,
+          height: 90,
+        },
+        headerTitleStyle: {
+          fontFamily: 'open-sans-semi-bold',
+        },
+
+        tabBarActiveTintColor: 'white',
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name='home-outline'
+                size={34}
+                color={
+                  focused
+                    ? GlobalStyles.colors.primary400
+                    : GlobalStyles.colors.primary500
+                }
+              />
+            );
           },
         }}
-      >
-        <Stack.Screen
-          name='WorkoutsScreen'
-          component={WorkoutsScreen}
-          options={{ title: 'Workouts' }}
-        />
-        <Stack.Screen
-          name='SelectPhase'
-          component={BlockOptions}
-          options={{ title: 'Training Phase' }}
-        />
-        <Stack.Screen
-          name='WorkoutSelection'
-          component={WorkoutSelection}
-          options={{
-            title: 'Select a workout',
-          }}
-        />
-        <Stack.Screen
-          name='SelectDay'
-          component={SelectDay}
-          options={{ title: 'Select Day' }}
-        />
-        <Stack.Screen name='WorkoutOfTheDay' component={WorkoutOfTheDay} />
+      />
+      <Tab.Screen
+        name='Workouts'
+        component={TrainingOptionsStack}
+        options={{
+          title: 'Workouts',
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name='barbell-outline'
+                size={34}
+                color={
+                  focused
+                    ? GlobalStyles.colors.primary400
+                    : GlobalStyles.colors.primary500
+                }
+              />
+            );
+          },
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name='RepMaxCalculator'
+        component={RepMaxCalculator}
+        options={{
+          title: 'Max Rep Calculator',
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name='calculator-outline'
+                size={30}
+                color={
+                  focused
+                    ? GlobalStyles.colors.primary400
+                    : GlobalStyles.colors.primary500
+                }
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name='ProgressChart'
+        component={ProgressChart}
+        options={{
+          title: 'Progress Chart',
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                name='bar-chart-outline'
+                size={30}
+                color={
+                  focused
+                    ? GlobalStyles.colors.primary400
+                    : GlobalStyles.colors.primary500
+                }
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
-        <Stack.Screen
-          name='PreviewModal'
-          component={PreviewModal}
-          options={{ presentation: 'modal', title: false }}
-        />
-      </Stack.Navigator>
-    );
-  }
+function RootApp() {
+  const { userIsAuthenticated } = useAppContext();
 
-  function AppNavigation() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: GlobalStyles.colors.background },
-            headerTintColor: 'white',
-            tabBarStyle: {
-              backgroundColor: GlobalStyles.colors.primary700,
-              paddingTop: 5,
-              height: 90,
-            },
-            headerTitleStyle: {
-              fontFamily: 'open-sans-semi-bold',
-            },
-
-            tabBarActiveTintColor: 'white',
-          }}
-        >
-          <Tab.Screen
-            name='Home'
-            component={HomeScreen}
-            options={{
-              title: 'Home',
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <Ionicons
-                    name='home-outline'
-                    size={34}
-                    color={
-                      focused
-                        ? GlobalStyles.colors.primary400
-                        : GlobalStyles.colors.primary500
-                    }
-                  />
-                );
-              },
-            }}
-          />
-          <Tab.Screen
-            name='Workouts'
-            component={TrainingOptionsStack}
-            options={{
-              title: 'Workouts',
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <Ionicons
-                    name='barbell-outline'
-                    size={34}
-                    color={
-                      focused
-                        ? GlobalStyles.colors.primary400
-                        : GlobalStyles.colors.primary500
-                    }
-                  />
-                );
-              },
-              headerShown: false,
-            }}
-          />
-          <Tab.Screen
-            name='RepMaxCalculator'
-            component={RepMaxCalculator}
-            options={{
-              title: 'Max Rep Calculator',
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <Ionicons
-                    name='calculator-outline'
-                    size={30}
-                    color={
-                      focused
-                        ? GlobalStyles.colors.primary400
-                        : GlobalStyles.colors.primary500
-                    }
-                  />
-                );
-              },
-            }}
-          />
-          <Tab.Screen
-            name='ProgressChart'
-            component={ProgressChart}
-            options={{
-              title: 'Progress Chart',
-              tabBarIcon: ({ focused }) => {
-                return (
-                  <Ionicons
-                    name='bar-chart-outline'
-                    size={30}
-                    color={
-                      focused
-                        ? GlobalStyles.colors.primary400
-                        : GlobalStyles.colors.primary500
-                    }
-                  />
-                );
-              },
-            }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
-  }
-
-  if (fontsLoaded && !userIsAuthenticated) {
-    return <AuthenticationStack />;
-  }
-
-  if (fontsLoaded && userIsAuthenticated) {
-    return <AppNavigation />;
-  }
+  return (
+    <NavigationContainer>
+      {!userIsAuthenticated && <AppNavigation />}
+      {userIsAuthenticated && <AuthenticationStack />}
+    </NavigationContainer>
+  );
 }
 
 export default function App() {
-  return (
-    <>
-      <StatusBar style='light' />
-      <AppContextProvider>
-        <RootApp />
-      </AppContextProvider>
-    </>
-  );
+  const [fontsLoaded] = useFonts(customFonts);
+
+  if (fontsLoaded)
+    return (
+      <>
+        <StatusBar style='light' />
+        <AppContextProvider>
+          <RootApp />
+        </AppContextProvider>
+      </>
+    );
 }

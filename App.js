fca48f0f314df707b01b18be0ea55bad14f0d3fa
DonUggioni <renan.uggioni@gmail.com>
2023-03-29 11:35:36 +0100

    Minor change

--- App.js
+++ App.js
@@ -261,14 +261,14 @@ export default function App() {
     setTimeout(() => {
       SplashScreen.hideAsync();
     }, 2000);
-
-    return (
-      <>
-        <StatusBar style='light' />
-        <AppContextProvider>
-          <RootApp />
-        </AppContextProvider>
-      </>
-    );
   }
+
+  return (
+    <>
+      <StatusBar style='light' />
+      <AppContextProvider>
+        <RootApp />
+      </AppContextProvider>
+    </>
+  );
 }
